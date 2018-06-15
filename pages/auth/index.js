dbf11a1564653f176a426824a60848479f99af39
jiraphon <jiraphon@sirimedia.com>
2018-06-15 17:02:35 +0700

    A Special  Lifecyle Hook

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authindexPages = () => (
+const authindexPages = (props) => (
   <div>
-    <h1>The Auth Index Page</h1>
+    <h1>The Auth Index Page - {props.appName}</h1>
     <User name="May" age={22} />
   </div>
 );
+
+authindexPages.getInitialProps = (context) => {
+  const promise = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      resolve({appName: 'Super App (Auth)'});
+    }, 1000);
+  });
+  return promise;
+};
 
 export default authindexPages;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import Link from 'next/link';
 import Router from "next/router";
 
-const indexPages = () => (
-  <div>
-    <h1>The Main Page</h1>
-    <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-    <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-  </div>
-);
+class IndexPages  extends Component {
 
-export default indexPages;
+  static async getInitialProps(context) {
+    const promise = new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        resolve({appName: 'Super App'});
+      }, 1000);
+    });
+    return promise;
+  }
+
+  render() {
+    return (
+    <div>
+      <h1>The Main Page of {this.props.appName}</h1>
+      <p>Go to {" "}<Link href="/auth"><a>Auth</a></Link></p>
+      <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+    </div>
+    );
+  }
+}
+
+export default IndexPages;
