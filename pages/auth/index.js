1511b31475db50c797f8a50c07ed523be67a4dee
jiraphon <jiraphon@sirimedia.com>
2018-06-15 14:59:59 +0700

    component & pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authindexPages = () => (
   <div>
     <h1>The Auth Index Page</h1>
+    <User name="May" age={22} />
   </div>
 );
 
